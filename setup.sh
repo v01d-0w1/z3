54b26f473ab4a56966502950cc1fd4be672f9562
z6death <tnz426.z6@gmail.com>
2025-07-16 23:12:12 +0600

    initial

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd z3
+cp -r layouts ~/.config/i3/
+cp -r scripts ~/.config/i3/
+cp -r workspace ~/
+cp config ~/.config/i3/
+cp config.bak ~/.config/i3/
+cp key.txt ~/.config/i3/
+cp lock.sh ~/.config/i3/ 
