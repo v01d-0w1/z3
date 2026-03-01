a0774ceee7a4f6ce3658beb015e9ee2c832c9760
z6 <tnz426.z6@gmail.com>
2026-03-01 15:31:01 +0600

    Initial commit

--- scripts/termdown_input.sh
+++ scripts/termdown_input.sh
@@ -6,7 +6,7 @@ read TIME
 if [ -n "$TIME" ]; then
     termdown "$TIME"
     # After termdown finishes - loop the audio
-    cvlc --no-video --loop ~/Music/jamtone.mp3
+    cvlc --no-video --loop ~/Music/'kaizen： becoming one percent better everyday ｜ a soothing playlist for focus time [OoSzt2Ga8Oc].mp3'
     ~/.config/i3/scripts/termdown_input.sh
 else
     echo "No time entered. Exiting..."
